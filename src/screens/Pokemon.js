8a931210acc7f97606c93b409fcdb98afdb01b4a
Jean Paul Tivan <jptivan53@gmail.com>
2023-03-29 23:06:01 -0400

    Screen de un pokemon

---
+++ src/screens/Pokemon.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Pokemon = () => {
+  return (
+    <View>
+      <Text>Página de un pokemon Pokemon</Text>
+    </View>
+  );
+};
+
+export default Pokemon;
