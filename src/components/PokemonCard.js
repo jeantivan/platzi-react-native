d11a38d54261a18e4337766c170c4a55ae9adfbc
Jean Paul Tivan <jptivan53@gmail.com>
2023-04-03 23:54:13 -0400

    Card para renderizar cada pokemon en la pokedex

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    height: 130,
+  },
+  spacing: {
+    flex: 1,
+    padding: 5,
+  },
+  bgStyles: {
+    backgroundColor: "gray",
+  },
+  order: {
+    position: "absolute",
+    right: 10,
+    top: 10,
+    color: "#fff",
+    fontSize: 11,
+  },
+  name: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 15,
+    paddingTop: 15,
+    paddingLeft: 5,
+  },
+  image: {
+    position: "absolute",
+    bottom: 2,
+    right: 2,
+    width: 90,
+    height: 90,
+  },
+});
+
+export default function PokemonCard({ pokemon }) {
+  const goToPokemon = () => {
+    console.log(`Vamos a pokemon: ${pokemon.name}`);
+  };
+  return (
+    <TouchableWithoutFeedback onPress={goToPokemon}>
+      <View style={styles.card}>
+        <View style={styles.spacing}>
+          <View style={styles.bgStyles}>
+            <Text style={styles.order}>
+              #{`${pokemon.order}`.padStart(3, 0)}
+            </Text>
+            <Text style={styles.name}>{pokemon.name}</Text>
+            <Image source={{ uri: pokemon.image }} style={styles.image} />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
