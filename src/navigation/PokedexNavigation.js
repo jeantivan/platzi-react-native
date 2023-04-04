2d5a8cb203970e612323873384eb102dcbe93c8b
Jean Paul Tivan <jptivan53@gmail.com>
2023-04-04 00:27:16 -0400

    Se oculta el header en esta pagina

--- src/navigation/PokedexNavigation.js
+++ src/navigation/PokedexNavigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import Pokedex from "../screens/Pokedex";
 import Pokemon from "../screens/Pokemon";
 
@@ -8,7 +9,11 @@ const Stack = createNativeStackNavigator();
 const PokedexNavigation = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Pokedex" component={Pokedex} />
+      <Stack.Screen
+        name="Pokedex"
+        component={Pokedex}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen name="Pokemon" component={Pokemon} />
     </Stack.Navigator>
   );
