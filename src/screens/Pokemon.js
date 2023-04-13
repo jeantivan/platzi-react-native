52d322865de635a3eb1e1a65ac6a91c600781de7
Jean Paul Tivan <jptivan53@gmail.com>
2023-04-13 19:21:29 -0400

    Obtiene la información del pokemon

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,10 +1,33 @@
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
-import React from "react";
 
-const Pokemon = () => {
+import { API_HOST } from "../utils/constants";
+import { getPokemonDetails } from "../api/pokemon";
+
+const Pokemon = ({ navigation, route }) => {
+  const { id } = route.params;
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    const loadPokemon = async () => {
+      try {
+        const response = await getPokemonDetails(`${API_HOST}/pokemon/${id}`);
+
+        setPokemon(response);
+      } catch (error) {
+        navigation.goBack();
+      }
+    };
+
+    loadPokemon();
+  }, []);
+
+  if (!pokemon) return null;
+
   return (
     <View>
-      <Text>Página de un pokemon Pokemon</Text>
+      <Text>Página de pokemon con id: {id}</Text>
+      <Text>Pokemon: {pokemon.name}</Text>
     </View>
   );
 };
