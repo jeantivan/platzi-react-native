a06b396eade65b75154e743ccec2a91f8b4007ab
jeantivan <jptivan53@gmail.com>
2023-04-22 18:16:16 -0400

    Mock data para el login de un usuario

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+const user = {
+  username: "johndoe",
+  password: "0123456789",
+};
+
+const userData = {
+  username: "johndoe",
+  firstName: "John",
+  lastName: "Doe",
+  email: "johndoe@gmail.com",
+};
