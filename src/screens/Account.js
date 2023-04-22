cc4f47ce389ee795c863d2db4516d3426d4121c0
jeantivan <jptivan53@gmail.com>
2023-04-22 18:18:10 -0400

    Mostrará el formulario de Login o la info del usuario

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+import UserData from "../components/Auth/UserData";
+import LoginForm from "../components/Auth/LoginForm";
+
 const Account = () => {
-  return (
-    <View>
-      <Text>Account</Text>
-    </View>
-  );
+  const auth = null;
+  return <View>{auth ? <UserData /> : <LoginForm />}</View>;
 };
 
 export default Account;
